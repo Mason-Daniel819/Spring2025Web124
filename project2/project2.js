8d4ee137e19e8ef0034812a07494ec6662391a19
Mason-Daniel819 <dmason21@stumail.jccc.edu>
2025-03-08 14:02:41 -0600

    Create project2.js

---
+++ project2/project2.js
@@ -0,0 +1,21 @@
+//Daniel Mason 03/06/2025
+let myName = 'Daniel Mason';
+console.log(myName);
+let para1 = document.getElementById('p1');
+para1.textContent = myName;
+let n1 = 6;
+let n2 = 12;
+let numberSum = (n1 + n2);
+let para2 = document.getElementById('p2').textContent = numberSum;
+let numberMult = (n1 * n2)
+let para3 = document.getElementById('p3').textContent = numberMult;
+let myNameAddNum = (myName + n1);
+let para4 = document.getElementById('p4').textContent = myNameAddNum;
+let myNameMultNum = (myName * n2);
+let para5 = document.getElementById('p5').textContent = myNameMultNum;
+let myAge = 34
+let ageCompare = myAge > n1;
+let para6 = document.getElementById('p6').textContent = ageCompare;
+
+n1 = 35;
+n2 = 27;
